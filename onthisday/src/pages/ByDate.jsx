e7683cb901f7efa3fa4a6a7ee62d92ae083dfa0f
Speks2 <142408255+Speks2@users.noreply.github.com>
2024-09-20 10:45:53 +0200

    it works

--- onthisday/src/pages/ByDate.jsx
+++ onthisday/src/pages/ByDate.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
-import styles from '../pages/ByDate.module.scss';
+import styles from '../pages/Today.module.scss';
+import { useState, useEffect } from 'react';
 
-export const ByDate = () => {
+export function ByDate() {
+  const [events, setEvents] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    //Fetcheted data from API!!!
+    fetch('https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/events/09/18')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setEvents(data.events);
+      })
+      .catch((error) => {
+        setError(error.toString());
+      });
+  }, []);
+
   return (
-    <div className={styles.ByDate}>
-        <h1>By Date</h1>
+    <div className={styles.Today}>
+      <h1>By Date</h1>
+
+      {error && <p>Error: {error}</p>}
+
+      <ul>
+        {events.map((event, index) => (
+          <li key={index}>
+            <strong>{event.year}</strong>: {event.text}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
